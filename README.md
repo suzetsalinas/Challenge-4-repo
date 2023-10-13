290e790341e6055040897a037c7afb3af376fc4e
Suzet Salinas <suzet.sal@gmail.com>
2023-10-12 19:09:11 -0500

    added timer, fixed question options

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Challenge-4-repo
-This is my assignment for Challenge 4 in which I create a timed quiz program that quizzes me on JavaScript fundamentals. It has a set time for the user to take the quiz, but decreases the time after incorrect answers. When either the time runs out or the user answers all questions, the user's initials and score will be saved and displayed.
+This is my assignment for Challenge 4 in which I create a timed quiz program that quizzes the user on JavaScript fundamentals. It has a set time  of 60 seconds for the user to take the quiz. When either the time runs out or the user answers all questions, the user can see their score! This project was quite difficult for me at this point in my coding career, so I sought out support and inspiration from colleagues as well as websites like w3schools.com and codewithfaraz.com.
+
+![Screenshot of application.](image.png)
--- script.js
+++ script.js
@@ -1,16 +1,3 @@
-// will each question be in a card?
-// 1. need a landing page with a start button for user to click on and trigger the first question
-// need an event listener for the start button
-// need a link at the top left to show stored scores
-// need a timer at the top right
-
-// 2. display first question, answer choices should be in bubbles, color changes with hover
-// user clicks on answer choice, if correct, continue with questions; if incorrect, decrease time by x increment, 
-
-// 3. continue to next question. show at bottom if user was correct/incorrect
-
-
-
 // data used for each question's card
 const quizData = [
     {
@@ -41,136 +28,133 @@ const quizData = [
     
   ];
 
-  const quizContainer = document.getElementById('quiz');
-  const resultContainer = document.getElementById('results');
-  const submitButton = document.getElementById('submit');
-  // retry will be used to loop back quiz and start over
-  const retestButton = document.getElementById('retest');
-  // showAnswer can be used to store score?
-  const showScoresButton = document.getElementById('showScore');
+// grabbing ID elements from the document and storing them in constants
+const quizContainer = document.getElementById('quiz');
+const resultContainer = document.getElementById('results');
+const submitButton = document.getElementById('submit');
+const retestButton = document.getElementById('retest');
+const showScoresButton = document.getElementById('showScore');
+const timerElement = document.getElementById('timer-value');
 
-  let currentQuestion = 0;
-  let score = 0;
-  // TODO: GET RID OF THIS ARRAY
-  let incorrectAnswers = [];
-// TODO: GET RID OF SHUFFLEARRAY FX
-  function shuffleArray(array) {
-    for (let i = array.length - 1; i > 0; i--) {
-      const j = Math.floor(Math.random() * (i + 1));
-      [array[i], array[j]] = [array[j], array[i]];
-    }
-  }
-  
-// TODO ADD FX TO SHOW STARTING CARD/PAGE BEFORE BEGGINNING QUIZ, FIX displayQuestion call 
-// at end of program to start with this new fx, ensure it cascades thru all other fx's
+// initializing quiz
+let currentQuestion = 0;
+let score = 0;
+let incorrectAnswers = [];
+let timer;
+let timeLeft = 60;
 
-  function displayQuestion() {
-    const questionData = quizData[currentQuestion];
+
+// Fx used to show each question and its answer choices, calls timer Fx
+function displayQuestion() {
+  const questionData = quizData[currentQuestion];
+
+  const questionElement = document.createElement('div');
+  questionElement.className = 'question';
+  questionElement.innerHTML = questionData.question;
+
+  const optionsElement = document.createElement('div');
+  optionsElement.className = 'options';
+
   
-    const questionElement = document.createElement('div');
-    questionElement.className = 'question';
-    questionElement.innerHTML = questionData.question;
-  
-    const optionsElement = document.createElement('div');
-    optionsElement.className = 'options';
-  
-    const shuffledOptions = [...questionData.options];
-    shuffleArray(shuffledOptions);
-  
-    for (let i = 0; i < shuffledOptions.length; i++) {
-      const option = document.createElement('label');
-      option.className = 'option';
-  
-      const radio = document.createElement('input');
-      radio.type = 'radio';
-      radio.name = 'quiz';
-      radio.value = shuffledOptions[i];
-  
-      const optionText = document.createTextNode(shuffledOptions[i]);
-  
-      option.appendChild(radio);
-      option.appendChild(optionText);
-      optionsElement.appendChild(option);
-    }
-  
-    quizContainer.innerHTML = '';
-    quizContainer.appendChild(questionElement);
-    quizContainer.appendChild(optionsElement);
-  }
-  
-  function checkAnswer() {
-    const selectedOption = document.querySelector('input[name="quiz"]:checked');
-    if (selectedOption) {
-      const answer = selectedOption.value;
-      if (answer === quizData[currentQuestion].answer) {
-        score++;
-      } else {
-        incorrectAnswers.push({
-          question: quizData[currentQuestion].question,
-          incorrectAnswer: answer,
-          correctAnswer: quizData[currentQuestion].answer,
-        });
-      }
-      currentQuestion++;
-      selectedOption.checked = false;
-      if (currentQuestion < quizData.length) {
-        displayQuestion();
-      } else {
-        displayResult();
-      }
-    }
+
+  for (let i = 0; i < questionData.options.length; i++) {
+    const option = document.createElement('label');
+    option.className = 'option';
+
+    const radio = document.createElement('input');
+    radio.type = 'radio';
+    radio.name = 'quiz';
+    
+
+    const optionText = document.createTextNode(questionData.options[i]);
+
+    option.appendChild(radio);
+    option.appendChild(optionText);
+    optionsElement.appendChild(option);
   }
 
-  function displayResult() {
-    quizContainer.style.display = 'none';
-    submitButton.style.display = 'none';
-    retestButton.style.display = 'inline-block';
-    showScoresButton.style.display = 'inline-block';
-    resultContainer.innerHTML = `You scored ${score} out of ${quizData.length}!`;
-  }
-  
-  function retryQuiz() {
-    currentQuestion = 0;
-    score = 0;
-    incorrectAnswers = [];
-    quizContainer.style.display = 'block';
-    submitButton.style.display = 'inline-block';
-    retestButton.style.display = 'none';
-    showScoresButton.style.display = 'none';
-    resultContainer.innerHTML = '';
-    displayQuestion();
-  }
-  
-  function showScores() {
-    quizContainer.style.display = 'none';
-    submitButton.style.display = 'none';
-    retestButton.style.display = 'inline-block';
-    showScoresButton.style.display = 'none';
-  
-    let incorrectAnswersHtml = '';
-    for (let i = 0; i < incorrectAnswers.length; i++) {
-      incorrectAnswersHtml += `
-        <p>
-          <strong>Question:</strong> ${incorrectAnswers[i].question}<br>
-          <strong>Your Answer:</strong> ${incorrectAnswers[i].incorrectAnswer}<br>
-          <strong>Correct Answer:</strong> ${incorrectAnswers[i].correctAnswer}
-        </p>
-      `;
+  quizContainer.innerHTML = '';
+  quizContainer.appendChild(questionElement);
+  quizContainer.appendChild(optionsElement);
+
+  startTimer();
+}
+
+// Fx for timer purposes 
+function startTimer() {
+  timer = setInterval(function () {
+    if (timeLeft <= 0) {
+      clearInterval(timer);
+      timeLeft = 0;
+      displayResult();
+    } else {
+      timerElement.textContent = timeLeft;
+      timeLeft--;
     }
-  
-    resultContainer.innerHTML = `
-      <p>You scored ${score} out of ${quizData.length}!</p>
-      <p>Incorrect Answers:</p>
-      ${incorrectAnswersHtml}
-    `;
+  }, 1000);
+}
+
+function stopTimer() {
+  clearInterval(timer);
+}
+
+function checkAnswer() {
+  const selectedOption = document.querySelector('input[name="quiz"]:checked');
+  if (selectedOption) {
+    const answer = selectedOption.value;
+    if (answer === quizData[currentQuestion].answer) {
+      score++;
+    } else {
+      incorrectAnswers.push({
+        question: quizData[currentQuestion].question,
+        incorrectAnswer: answer,
+        correctAnswer: quizData[currentQuestion].answer,
+      });
+    }
+    currentQuestion++;
+    selectedOption.checked = false;
+    if (currentQuestion < quizData.length) {
+      displayQuestion();
+    } else {
+      displayResult();
+    }
   }
+}
+
+function displayResult() {
+  stopTimer();
+  quizContainer.style.display = 'none';
+  submitButton.style.display = 'none';
+  retestButton.style.display = 'inline-block';
+  showScoresButton.style.display = 'none';
+  resultContainer.innerHTML = `You scored ${score} out of ${quizData.length}!`;
+}
+
+function retryQuiz() {
+  startTimer();
+  currentQuestion = 0;
+  score = 0;
+  incorrectAnswers = [];
+  quizContainer.style.display = 'block';
+  submitButton.style.display = 'inline-block';
+  retestButton.style.display = 'none';
+  showScoresButton.style.display = 'none';
+  resultContainer.innerHTML = '';
+  displayQuestion();
+}
+
+function showScores() {
+  quizContainer.style.display = 'none';
+  submitButton.style.display = 'none';
+  retestButton.style.display = 'inline-block';
+  showScoresButton.style.display = 'none';
+
+}
 
 
 
 submitButton.addEventListener('click', checkAnswer);
 retestButton.addEventListener('click', retryQuiz);
-// showScoreButton can be used to show saved scores
-// TODO: FIGURE OUT HOW TO SAVE SCORES
 showScoresButton.addEventListener('click', showScores);
 
 displayQuestion();
